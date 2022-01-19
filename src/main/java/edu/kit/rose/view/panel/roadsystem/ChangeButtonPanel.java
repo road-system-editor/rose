3f955799ba9fd6d87ea86de5f14253b53c874fba
cgoru <cgorun2000@gmail.com>
2022-01-19 12:06:33 +0100

    Fix checkstyle for view.panel.roadsystem

--- src/main/java/edu/kit/rose/view/panel/roadsystem/ChangeButtonPanel.java
+++ src/main/java/edu/kit/rose/view/panel/roadsystem/ChangeButtonPanel.java
@@ -8,21 +8,24 @@ import edu.kit.rose.view.commons.FXMLContainer;
 /**
  * The change button panel contains the buttons for undoing and redoing a changeable action.
  */
+@SuppressWarnings("checkstyle:SummaryJavadoc")
 public class ChangeButtonPanel extends FXMLContainer {
   private ApplicationController controller;
 
   /**
-   * Creates a new FXMLPanel and immediately mounts the components specified in the given FXML file ({@code fxmlResourceName}.
-   * Requires {@link #setTranslator(LocalizedTextProvider)} + {@link #setController(ApplicationController)}
+   * Creates a new FXMLPanel and immediately mounts the components
+   * specified in the given FXML file ({@code fxmlResourceName}.
+   * Requires {@link #setTranslator(LocalizedTextProvider)} +
+   * {@link #setController(ApplicationController)}
    */
   public ChangeButtonPanel() {
     super("change_button_panel.fxml");
   }
 
   /**
-   * Sets the application controller
+   * Sets the application controller.
    *
-   * @param controller
+   * @param controller the application controller
    */
   public void setController(ApplicationController controller) {
     this.controller = controller;

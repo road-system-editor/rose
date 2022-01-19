3c113b0e73673dc09b77edf7fee99f25f87c1d07
Ztrew <Janneshelmke@gmail.com>
2022-01-19 16:38:29 +0100

    Implemented SimpleBox and SimpleSortedBox. Implemented tests for them.

--- src/main/java/edu/kit/rose/infrastructure/SimpleBox.java
+++ src/main/java/edu/kit/rose/infrastructure/SimpleBox.java
@@ -1,21 +1,35 @@
 package edu.kit.rose.infrastructure;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 /**
- * A standard implementation of a Box.
+ * A standard implementation of a {@link Box}.
  *
  * @param <T> The Type of the Objects in the Box.
  */
 public class SimpleBox<T> implements Box<T> {
 
+  private final List<T> content;
+
+  /**
+   * Constructor.
+   *
+   * @param content the Elements that are supposed to be in the Box.
+   */
+  public SimpleBox(Collection<T> content) {
+    this.content = new ArrayList<>(content);
+  }
+
   @Override
   public int getSize() {
-    return 0;
+    return content.size();
   }
 
   @Override
   public Iterator<T> iterator() {
-    return null;
+    return content.iterator();
   }
 }

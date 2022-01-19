848e4fa2b513b5198ddd265adcc84a7ad2d07243
Max Schweikart <hello@maxschweik.art>
2022-01-19 19:58:31 +0100

    docs: merge updated view.panel.segmentbox docs from design doc

---
+++ src/main/java/edu/kit/rose/view/panel/segmentbox/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package exposes the {@link edu.kit.rose.view.panel.segmentbox.SegmentBoxPanel} class
+ * which provides an overview over the available segment types and allows the user to create new
+ * segments in the road system.
+ */
+package edu.kit.rose.view.panel.segmentbox;

6e695b8a67f1fc8cdac2fcdabcece397a78f703b
YS <yannik@d-soft.de>
2022-01-21 11:23:12 +0100

    RoseStorageLockTest checkstyle errors fixed.

--- src/test/java/edu/kit/rose/controller/commons/RoseStorageLockTest.java
+++ src/test/java/edu/kit/rose/controller/commons/RoseStorageLockTest.java
@@ -3,18 +3,21 @@ package edu.kit.rose.controller.commons;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests the {@link RoseStorageLock}.
+ */
 public class RoseStorageLockTest {
 
-    @Test
-    public void testAcquiring() {
-        RoseStorageLock storageLock = new RoseStorageLock();
+  @Test
+  public void testAcquiring() {
+    RoseStorageLock storageLock = new RoseStorageLock();
 
-        Assertions.assertFalse(storageLock.isStorageLockAcquired());
+    Assertions.assertFalse(storageLock.isStorageLockAcquired());
 
-        storageLock.acquireStorageLock();
-        Assertions.assertTrue(storageLock.isStorageLockAcquired());
+    storageLock.acquireStorageLock();
+    Assertions.assertTrue(storageLock.isStorageLockAcquired());
 
-        storageLock.releaseStorageLock();
-        Assertions.assertFalse(storageLock.isStorageLockAcquired());
-    }
+    storageLock.releaseStorageLock();
+    Assertions.assertFalse(storageLock.isStorageLockAcquired());
+  }
 }

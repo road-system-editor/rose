78ebfab188acdb78070573dcaf081f915d800373
YS <yannik@d-soft.de>
2022-01-21 11:13:51 +0100

    RoseBoxTest checkstyle errors fixed.

--- src/test/java/edu/kit/rose/infrastructure/RoseBoxTest.java
+++ src/test/java/edu/kit/rose/infrastructure/RoseBoxTest.java
@@ -1,45 +1,46 @@
 package edu.kit.rose.infrastructure;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 
 /**
  * Unit Test for the RoseBox Class.
  */
 public class RoseBoxTest {
-    static int numberOfElements = 10;
-    static Collection<Integer> testArrayList;
+  static int numberOfElements = 10;
+  static Collection<Integer> testArrayList;
 
-    @BeforeAll
-    static void setup() {
-        testArrayList = new ArrayList<>();
-        for (int i = 0; i < numberOfElements; i++) {
-            testArrayList.add(i);
-        }
+  @BeforeAll
+  static void setup() {
+    testArrayList = new ArrayList<>();
+    for (int i = 0; i < numberOfElements; i++) {
+      testArrayList.add(i);
     }
-    @Test
-    public void TestGetSize() {
-        Box<Integer> box = new SimpleBox<>(testArrayList);
-        Assertions.assertEquals(numberOfElements, box.getSize());
+  }
+
+  @Test
+  public void testGetSize() {
+    Box<Integer> box = new SimpleBox<>(testArrayList);
+    Assertions.assertEquals(numberOfElements, box.getSize());
+  }
+
+  @Test
+  public void testIterator() {
+
+    Box<Integer> box = new SimpleBox<>(testArrayList);
+    Iterator<Integer> iterator = box.iterator();
+    Collection<Integer> actualContent = new ArrayList<>();
+
+    for (int i = 0; i < numberOfElements; i++) {
+      actualContent.add(iterator.next());
     }
-
-    @Test
-    public void TestIterator() {
-
-        Box<Integer> box = new SimpleBox<>(testArrayList);
-        Iterator<Integer> iterator = box.iterator();
-        Collection<Integer> actualContent = new ArrayList<>();
-
-        for(int i = 0; i < numberOfElements; i++) {
-            actualContent.add(iterator.next());
-        }
-        for(int i = 0; i < numberOfElements; i++) {
-            Assertions.assertTrue(actualContent.contains(i));
-        }
+    for (int i = 0; i < numberOfElements; i++) {
+      Assertions.assertTrue(actualContent.contains(i));
     }
+  }
 }

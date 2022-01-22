9f3ce75f7440fd0ca48c99a60435da1738a3d59b
cgoru <cgorun2000@gmail.com>
2022-01-22 16:56:12 +0100

    IMPLEMENTED unit tests for CreateGroupCommand and DeleteGroupCommand

---
+++ src/test/java/edu/kit/rose/controller/hierarchy/CreateGroupCommandTest.java
@@ -0,0 +1,65 @@
+package edu.kit.rose.controller.hierarchy;
+
+
+import edu.kit.rose.model.Project;
+import edu.kit.rose.model.SimpleProject;
+import edu.kit.rose.model.plausibility.criteria.CriteriaManager;
+import edu.kit.rose.model.roadsystem.GraphRoadSystem;
+import edu.kit.rose.model.roadsystem.RoadSystem;
+import edu.kit.rose.model.roadsystem.TimeSliceSetting;
+import edu.kit.rose.model.roadsystem.elements.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+class CreateGroupCommandTest {
+  private RoadSystem roadSystem;
+  private CriteriaManager criteriaManager;
+  private Group group;
+  private Project project;
+
+  @BeforeEach
+  public void setUp() {
+    this.group = null;
+    this.criteriaManager = new CriteriaManager();
+    this.project = new SimpleProject(criteriaManager) {
+      @Override
+      public RoadSystem getRoadSystem() {
+        return roadSystem;
+      }
+    };
+    this.roadSystem = new GraphRoadSystem(criteriaManager, new TimeSliceSetting()) {
+
+      @Override
+      public Group createGroup(Collection<Element> includedElements) {
+        group = new Group();
+        return group;
+      }
+
+      @Override
+      public void removeElement(Element element) {
+        group = null;
+      }
+    };
+  }
+
+  @Test
+  void executeTest() {
+    List<Element> elements = new ArrayList<>();
+    CreateGroupCommand command = new CreateGroupCommand(this.project, elements);
+    command.execute();
+    Assertions.assertNotEquals(null, this.group);
+  }
+
+  @Test
+  public void unexecuteTest() {
+    List<Element> elements = new ArrayList<>();
+    CreateGroupCommand command = new CreateGroupCommand(this.project, elements);
+    command.execute();
+    command.unexecute();
+    Assertions.assertNull(this.group);
+  }
+}
---
+++ src/test/java/edu/kit/rose/controller/hierarchy/DeleteGroupCommandTest.java
@@ -0,0 +1,153 @@
+package edu.kit.rose.controller.hierarchy;
+
+import edu.kit.rose.infrastructure.SimpleSortedBox;
+import edu.kit.rose.infrastructure.SortedBox;
+import edu.kit.rose.model.Project;
+import edu.kit.rose.model.SimpleProject;
+import edu.kit.rose.model.plausibility.criteria.CriteriaManager;
+import edu.kit.rose.model.roadsystem.GraphRoadSystem;
+import edu.kit.rose.model.roadsystem.RoadSystem;
+import edu.kit.rose.model.roadsystem.TimeSliceSetting;
+import edu.kit.rose.model.roadsystem.attributes.AttributeAccessor;
+import edu.kit.rose.model.roadsystem.attributes.AttributeType;
+import edu.kit.rose.model.roadsystem.elements.Base;
+import edu.kit.rose.model.roadsystem.elements.Element;
+import edu.kit.rose.model.roadsystem.elements.Group;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteGroupCommandTest {
+  private static final int EXPECTED_NUMBER_OF_ELEMENTS = 2;
+  private static final String UNSETED = "unseted";
+  private static final String SETED = "seted";
+  private List<Element> elements;
+  private Element element1;
+  private Element element2;
+  private RoadSystem roadSystem;
+  private CriteriaManager criteriaManager;
+  private Group mockGroup;
+  private Group group;
+  private Project project;
+  private AttributeAccessor<String> accessor;
+  private String name;
+
+
+  @BeforeEach
+  public void setUp() {
+    this.name = UNSETED;
+    this.elements = new ArrayList<>();
+    this.element1 = new Base();
+    this.element2 = new Base();
+    this.elements.add(element1);
+    this.elements.add(element2);
+    this.criteriaManager = new CriteriaManager();
+    this.name = UNSETED;
+    this.mockGroup = new Group() {
+      private final List<Element> auxElements = new ArrayList<>();
+
+      @Override
+      public SortedBox<AttributeAccessor<?>> getAttributeAccessors() {
+        List<AttributeAccessor<?>> list = new ArrayList<>();
+        list.add(accessor);
+        return new SimpleSortedBox<>(list);
+      }
+
+      @Override
+      public SortedBox<Element> getElements() {
+        return new SimpleSortedBox<>(this.auxElements);
+      }
+
+      @Override
+      public void addElement(Element element) {
+        this.auxElements.add(element);
+      }
+
+      @Override
+      public void removeElement(Element e) {
+        this.auxElements.remove(e);
+      }
+    };
+
+    this.group = this.mockGroup;
+    this.group.addElement(element1);
+    this.group.addElement(element2);
+
+    this.accessor = new AttributeAccessor<>() {
+      @Override
+      public AttributeType getAttributeType() {
+        return AttributeType.NAME;
+      }
+
+      @Override
+      public void setValue(String str) {
+        name = SETED;
+      }
+
+      @Override
+      public String getValue() {
+        return name;
+      }
+    };
+
+    this.project = new SimpleProject(criteriaManager) {
+      @Override
+      public RoadSystem getRoadSystem() {
+        return roadSystem;
+      }
+    };
+
+    this.roadSystem = new GraphRoadSystem(this.criteriaManager, new TimeSliceSetting()) {
+      @Override
+      public Group createGroup(Collection<Element> includedElements) {
+        mockGroup.removeElement(element1);
+        mockGroup.removeElement(element2);
+        group = mockGroup;
+        includedElements.forEach(group::addElement);
+        return group;
+      }
+
+      @Override
+      public void removeElement(Element element) {
+        group = null;
+      }
+    };
+  }
+
+  @Test
+  void execute() {
+    DeleteGroupCommand command = new DeleteGroupCommand(this.project, this.group);
+    command.execute();
+    assertNull(this.group);
+  }
+
+  @Test
+  void unexecute() {
+    DeleteGroupCommand command = new DeleteGroupCommand(this.project, this.group);
+    command.execute();
+    command.unexecute();
+
+    Assertions.assertEquals(this.mockGroup, this.group);
+    // checks if the unexecute method sets the same name as on removed group
+    Assertions.assertEquals(SETED, this.name);
+
+    SortedBox<Element> groupElements = this.group.getElements();
+    SortedBox<Element> expectedElements = new SimpleSortedBox<>(this.elements);
+
+    Assertions.assertEquals(EXPECTED_NUMBER_OF_ELEMENTS, groupElements.getSize());
+
+    Iterator<Element> groupIterator = groupElements.iterator();
+
+    // checks if the elements from deleted group where added to this group
+    for (Element expectedElement : expectedElements) {
+      Assertions.assertEquals(expectedElement, groupIterator.next());
+    }
+  }
+}

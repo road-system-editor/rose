ac14e347be2090ca0096bba662d2c453709dd832
PhilippSeidel00 <spam@philipp-seidel.de>
2022-01-22 23:06:14 +0100

    changed class types in method overrides in child classes of PlausibilityCriterion

--- src/main/java/edu/kit/rose/model/plausibility/criteria/CompatibilityCriterion.java
+++ src/main/java/edu/kit/rose/model/plausibility/criteria/CompatibilityCriterion.java
@@ -3,11 +3,10 @@ package edu.kit.rose.model.plausibility.criteria;
 import edu.kit.rose.infrastructure.Box;
 import edu.kit.rose.infrastructure.SetObserver;
 import edu.kit.rose.infrastructure.SortedBox;
-import edu.kit.rose.infrastructure.UnitObserver;
 import edu.kit.rose.model.plausibility.criteria.validation.OperatorType;
 import edu.kit.rose.model.roadsystem.RoadSystem;
 import edu.kit.rose.model.roadsystem.attributes.AttributeType;
-import edu.kit.rose.model.roadsystem.elements.Segment;
+import edu.kit.rose.model.roadsystem.elements.Element;
 import edu.kit.rose.model.roadsystem.elements.SegmentType;
 
 /**
@@ -122,7 +121,7 @@ public class CompatibilityCriterion implements PlausibilityCriterion {
   }
 
   @Override
-  public void notifyChange(Segment unit) {
+  public void notifyChange(Element unit) {
 
   }
 

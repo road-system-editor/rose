8f65e7bf848cff0e64872339e35d005e085e6436
YS <yannik@d-soft.de>
2022-01-23 17:47:28 +0100

    GroupView and SegmentView adjusted for switch from listview to treeview.

--- src/main/java/edu/kit/rose/view/commons/SearchBar.java
+++ src/main/java/edu/kit/rose/view/commons/SearchBar.java
@@ -4,7 +4,10 @@ import edu.kit.rose.infrastructure.UnitObservable;
 import edu.kit.rose.infrastructure.UnitObserver;
 import edu.kit.rose.infrastructure.language.Language;
 import edu.kit.rose.infrastructure.language.LocalizedTextProvider;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
@@ -13,30 +16,39 @@ import javafx.scene.control.TextField;
  * component.
  */
 public class SearchBar extends FxmlContainer implements UnitObservable<SearchBar> {
+
+  private final List<UnitObserver<SearchBar>> searchBarObserver = new ArrayList<>();
+
   @FXML
   private TextField searchTextField;
 
+  private final ChangeListener<? super String> textChangedCallback
+      = (o, e, n) -> notifySubscribers();
+
   /**
    * Creates a new search bar.
-   * Requires {@link #setTranslator(LocalizedTextProvider)}
    */
   public SearchBar() {
     super("SearchBar.fxml");
+
+    searchTextField.textProperty().addListener(textChangedCallback);
   }
 
   @Override
   public void addSubscriber(UnitObserver<SearchBar> observer) {
-
+    if (!searchBarObserver.contains(observer)) {
+      searchBarObserver.add(observer);
+    }
   }
 
   @Override
   public void removeSubscriber(UnitObserver<SearchBar> observer) {
-
+    searchBarObserver.remove(observer);
   }
 
   @Override
   public void notifySubscribers() {
-
+    searchBarObserver.forEach(observer -> observer.notifyChange(getThis()));
   }
 
   @Override
@@ -46,7 +58,17 @@ public class SearchBar extends FxmlContainer implements UnitObservable<SearchBar
 
   @Override
   protected void updateTranslatableStrings(Language lang) {
+    searchTextField.setPromptText(
+        getTranslator().getLocalizedText("view.commons.searchbar.watermark"));
+  }
 
+  /**
+   * Returns the current search string entered into the {@link SearchBar}.
+   *
+   * @return the current search string
+   */
+  public String getSearchString() {
+    return searchTextField.getText();
   }
 
   @Override

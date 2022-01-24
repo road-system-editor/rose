00996fbb48d72f2e34dba7721c589d993e96c8fd
Max Schweikart <hello@maxschweik.art>
2022-01-24 13:35:59 +0100

    feat(criteria): implement Violation

--- src/main/java/edu/kit/rose/model/plausibility/violation/Violation.java
+++ src/main/java/edu/kit/rose/model/plausibility/violation/Violation.java
@@ -4,38 +4,30 @@ import edu.kit.rose.model.plausibility.criteria.PlausibilityCriterion;
 import edu.kit.rose.model.roadsystem.elements.Connection;
 import edu.kit.rose.model.roadsystem.elements.Segment;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
 
 /**
  * A Violation is an offense against a {@link PlausibilityCriterion} caused by one {@link Segment}
  * or a {@link Connection} of multiple {@link Segment}s.
+ *
+ * @param violatedCriterion the criterion that this violation offends, may not be null.
+ * @param offendingSegments the {@link Segment}s that cause this violation, may not be null.
  */
-public class Violation {
+public record Violation(
+    PlausibilityCriterion violatedCriterion,
+    Collection<Segment> offendingSegments) {
 
   /**
-   * Constructor.
+   * Creates a new violation of the given criterion through the given segments.
    *
-   * @param violatedCriterion the {@link PlausibilityCriterion} this RoseViolation offends.
-   * @param offendingSegments the {@link Segment}s that cause this RoseViolation.
+   * @param violatedCriterion the criterion that this violation offends.
+   * @param offendingSegments the {@link Segment}s that cause this violation.
    */
   public Violation(PlausibilityCriterion violatedCriterion, Collection<Segment> offendingSegments) {
-
-  }
-
-  /**
-   * Provides the violated Criterion.
-   *
-   * @return The violated {@link PlausibilityCriterion}.
-   */
-  public PlausibilityCriterion getViolatedCriterion() {
-    return null;
-  }
-
-  /**
-   * Provides the Segments that caused this violation.
-   *
-   * @return The {@link Segment}s that caused this Violation.
-   */
-  public Collection<Segment> offendingSegments() {
-    return null;
+    this.violatedCriterion =
+        Objects.requireNonNull(violatedCriterion, "violated criterion may not be null");
+    this.offendingSegments = Collections.unmodifiableCollection(
+        Objects.requireNonNull(offendingSegments, "offending segment may not be null"));
   }
 }

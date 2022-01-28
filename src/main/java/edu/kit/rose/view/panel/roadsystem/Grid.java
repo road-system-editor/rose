210719de0459317ff6ea3bb0d93fc9b52ad5c232
PhilippSeidel00 <spam@philipp-seidel.de>
2022-01-28 22:11:37 +0100

    implemented grid

--- src/main/java/edu/kit/rose/view/panel/roadsystem/Grid.java
+++ src/main/java/edu/kit/rose/view/panel/roadsystem/Grid.java
@@ -2,13 +2,38 @@ package edu.kit.rose.view.panel.roadsystem;
 
 import edu.kit.rose.model.roadsystem.elements.Segment;
 import edu.kit.rose.view.commons.SegmentView;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
 
 /**
  * Represents a background surface that shows a grid, on which segment views can be drawn.
  */
 public class Grid extends Pane {
+
+  private static final int HEIGHT = 3000;
+  private static final int WIDTH = 3000;
+  private static final int HORIZONTAL_LINE_SPACING = 5;
+  private static final int VERTICAL_LINE_SPACING = 5;
+  private static final Color BACKGROUND_COLOR = Color.gray(0.95);
+  private static final Color LINE_COLOR = Color.gray(0.7);
+  private static final float LINE_WIDTH = 0.5f;
+
+  /**
+   * creates new Grid.
+   */
+  public Grid() {
+    setWidth(WIDTH);
+    setHeight(HEIGHT);
+    setBackground(new Background(new BackgroundFill(BACKGROUND_COLOR, null, null)));
+    getChildren().addAll(getLines());
+  }
 
   /**
    * Contains all displayed segment views on grid.
@@ -29,5 +54,39 @@ public class Grid extends Pane {
    * @param segmentView the segment view to remove
    */
   public void removeSegmentView(SegmentView<? extends Segment> segmentView) {
+  }
+
+  private Collection<Line> getLines() {
+    var lines = new LinkedList<Line>();
+    for (int y = 0; y < HEIGHT; y += HORIZONTAL_LINE_SPACING) {
+      lines.add(getLine(y, Orientation.HORIZONTAL));
+    }
+    for (int x = 0; x < WIDTH; x += VERTICAL_LINE_SPACING) {
+      lines.add(getLine(x, Orientation.VERTICAL));
+    }
+    return lines;
+  }
+
+  private Line getLine(int offset, Orientation orientation) {
+    var line = new Line();
+    if (orientation == Orientation.HORIZONTAL) {
+      line.setStartX(0);
+      line.setStartY(offset);
+      line.setEndX(WIDTH);
+      line.setEndY(offset);
+    } else {
+      line.setStartX(offset);
+      line.setStartY(0);
+      line.setEndX(offset);
+      line.setEndY(HEIGHT);
+    }
+    line.setFill(LINE_COLOR);
+    line.setStroke(LINE_COLOR);
+    line.setStrokeWidth(LINE_WIDTH);
+    return line;
+  }
+
+  private enum Orientation {
+    HORIZONTAL, VERTICAL
   }
 }

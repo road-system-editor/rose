378b2e88c91291c82ed93e2eeb2aff90dde06476
YS <yannik@d-soft.de>
2022-01-28 08:26:50 +0100

    Added missing  controller methods.

---
+++ src/main/java/edu/kit/rose/controller/navigation/FileDialogOption.java
@@ -0,0 +1,10 @@
+package edu.kit.rose.controller.navigation;
+
+/**
+ * Contains options that configure a call of {@link Navigator}s
+ * showFileDialog method.
+ */
+public enum FileDialogOptions {
+  LOAD_FILE,
+  SAVE_FILE
+}

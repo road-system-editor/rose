e39158db0e211e30c4512b4f79da4536d8850b6a
YS <yannik@d-soft.de>
2022-01-30 11:41:34 +0100

    Improved style in ShorCutHelpWindow and changed UI to be unmodifiable.

--- src/main/java/edu/kit/rose/view/window/ShortCutHelpWindow.java
+++ src/main/java/edu/kit/rose/view/window/ShortCutHelpWindow.java
@@ -5,12 +5,14 @@ import edu.kit.rose.infrastructure.language.Language;
 import edu.kit.rose.view.commons.FxmlUtility;
 import java.util.List;
 import java.util.function.Consumer;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Border;
 import javafx.stage.Stage;
 
 /**
@@ -18,6 +20,8 @@ import javafx.stage.Stage;
  */
 public class ShortCutHelpWindow extends RoseWindow {
 
+  private static final int WIDTH = 385;
+  private static final int HEIGHT = 385;
   private static final List<String> shortCutLocalizationKeys =
       List.of(
           "view.window.shortCutHelp.save.",
@@ -56,18 +60,50 @@ public class ShortCutHelpWindow extends RoseWindow {
   protected void configureStage(Stage stage, Injector injector) {
     Parent tree = FxmlUtility.loadFxml(null, this,
         getClass().getResource("ShortCutHelpWindow.fxml"));
-    var scene = new Scene(tree);
+    Scene scene = new Scene(tree);
     stage.setScene(scene);
-    stage.setWidth(385);
-    stage.setHeight(385);
+    stage.setWidth(WIDTH);
+    stage.setHeight(HEIGHT);
     stage.setResizable(false);
+    updateTranslatableStrings(stage);
+
+    configureTranslatorSubscription(stage);
+    setupTable();
+  }
+
+  private void configureTranslatorSubscription(Stage stage) {
     languageConsumer = language -> updateTranslatableStrings(stage);
     getTranslator().subscribeToOnLanguageChanged(languageConsumer);
-    setupTable();
+
     stage.setOnCloseRequest(
         event -> getTranslator().unsubscribeFromOnLanguageChanged(languageConsumer));
-    updateTranslatableStrings(stage);
   }
+
+  private void setupTable() {
+    shortCutTable.setBorder(Border.EMPTY);
+    shortCutTable.setPadding(Insets.EMPTY);
+    shortCutTable.setSelectionModel(null);
+
+    shortCutColumn = new TableColumn<>();
+    shortCutColumn.setReorderable(false);
+    shortCutColumn.setResizable(false);
+    shortCutColumn.setSortable(false);
+    shortCutColumn.setCellValueFactory(columnEntry ->
+        new SimpleStringProperty(columnEntry.getValue().shortCut()));
+    shortCutTable.getColumns().add(shortCutColumn);
+
+    descriptionColumn = new TableColumn<>();
+    descriptionColumn.setReorderable(false);
+    descriptionColumn.setResizable(false);
+    descriptionColumn.setSortable(false);
+    descriptionColumn.setCellValueFactory(columnEntry ->
+        new SimpleStringProperty(columnEntry.getValue().description()));
+    shortCutTable.getColumns().add(descriptionColumn);
+
+    descriptionColumn.prefWidthProperty()
+        .bind(shortCutTable.widthProperty().subtract(shortCutColumn.getWidth()));
+  }
+
 
   private void updateTranslatableStrings(Stage stage) {
     stage.setTitle(getTranslator().getLocalizedText("view.window.shortCutHelp.title"));
@@ -84,31 +120,6 @@ public class ShortCutHelpWindow extends RoseWindow {
     )));
   }
 
-  private void setupTable() {
-    shortCutColumn = new TableColumn<>();
-    descriptionColumn = new TableColumn<>();
-    shortCutColumn.setCellValueFactory(new PropertyValueFactory<>("shortCut"));
-    descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
-    shortCutTable.getColumns().addAll(shortCutColumn, descriptionColumn);
-  }
 
-  //TODO: fix public
-  public class ShortcutTableEntry {
-
-    private final String shortCut;
-    private final String description;
-
-    public ShortcutTableEntry(String shortCut, String description) {
-      this.shortCut = shortCut;
-      this.description = description;
-    }
-
-    public String getDescription() {
-      return description;
-    }
-
-    public String getShortCut() {
-      return shortCut;
-    }
-  }
+  private record ShortcutTableEntry(String shortCut, String description) { }
 }

872d8c6df2d18bc61fdfddf9271257e93f0818e4
cgoru <cgorun2000@gmail.com>
2022-02-04 17:30:53 +0100

    checkstyle fix

--- src/test/java/edu/kit/rose/controller/roadsystem/RoseRoadSystemControllerTest.java
+++ src/test/java/edu/kit/rose/controller/roadsystem/RoseRoadSystemControllerTest.java
@@ -10,13 +10,17 @@ import edu.kit.rose.infrastructure.RoseBox;
 import edu.kit.rose.model.Project;
 import edu.kit.rose.model.ZoomSetting;
 import edu.kit.rose.model.roadsystem.RoadSystem;
-
+import edu.kit.rose.model.roadsystem.elements.Base;
+import edu.kit.rose.model.roadsystem.elements.Connector;
+import edu.kit.rose.model.roadsystem.elements.Element;
+import edu.kit.rose.model.roadsystem.elements.Entrance;
+import edu.kit.rose.model.roadsystem.elements.Exit;
+import edu.kit.rose.model.roadsystem.elements.Segment;
+import edu.kit.rose.model.roadsystem.elements.SegmentType;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
-
-import edu.kit.rose.model.roadsystem.elements.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;

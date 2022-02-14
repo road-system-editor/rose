888f5db617af26a477a949370210dcb22efff19d
YS <yannik@d-soft.de>
2022-02-14 17:54:58 +0100

    RoseProjectController all methods, but save, implemented.

--- src/main/java/edu/kit/rose/controller/project/RoseProjectController.java
+++ src/main/java/edu/kit/rose/controller/project/RoseProjectController.java
@@ -2,12 +2,15 @@ package edu.kit.rose.controller.project;
 
 import edu.kit.rose.controller.commons.Controller;
 import edu.kit.rose.controller.commons.StorageLock;
+import edu.kit.rose.controller.navigation.ErrorType;
 import edu.kit.rose.controller.navigation.FileDialogType;
 import edu.kit.rose.controller.navigation.FileFormat;
 import edu.kit.rose.controller.navigation.Navigator;
 import edu.kit.rose.model.Project;
 import edu.kit.rose.model.ProjectFormat;
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Provides functionality to save, load and
@@ -16,6 +19,9 @@ import java.nio.file.Path;
 public class RoseProjectController extends Controller implements ProjectController {
 
   private final Project project;
+
+  private final Set<Runnable> onProjectIoActionBeginCallbacks;
+  private final Set<Runnable> onProjectIoActionEndCallbacks;
 
   /**
    * Creates a new {@link RoseProjectController}.
@@ -28,39 +34,86 @@ public class RoseProjectController extends Controller implements ProjectControll
     super(storageLock, navigator);
 
     this.project = project;
+    this.onProjectIoActionBeginCallbacks = new HashSet<>();
+    this.onProjectIoActionEndCallbacks = new HashSet<>();
   }
 
   @Override
   public void export(ProjectFormat targetFormat) {
+    if (!getStorageLock().isStorageLockAcquired()) {
+      getStorageLock().acquireStorageLock();
+      this.onProjectIoActionBeginCallbacks.forEach(Runnable::run);
+      FileFormat targetFileFormat = switch (targetFormat) {
+        case ROSE -> FileFormat.ROSE;
+        case YAML -> FileFormat.YAML;
+        case SUMO -> FileFormat.SUMO;
+      };
+      Path targetFilePath
+          = getNavigator().showFileDialog(FileDialogType.SAVE_FILE, targetFileFormat);
 
+      if (targetFilePath != null) {
+        boolean exportSucceeded = project.exportToFile(targetFormat, targetFilePath);
+        if (!exportSucceeded) {
+          getNavigator().showErrorDialog(ErrorType.EXPORT_ERROR);
+        }
+      }
+      this.onProjectIoActionEndCallbacks.forEach(Runnable::run);
+      getStorageLock().releaseStorageLock();
+    }
   }
 
   @Override
   public void save() {
+    if (!getStorageLock().isStorageLockAcquired()) {
+      getStorageLock().acquireStorageLock();
+      this.onProjectIoActionBeginCallbacks.forEach(Runnable::run);
 
+      this.onProjectIoActionEndCallbacks.forEach(Runnable::run);
+    }
   }
 
   @Override
   public void saveAs() {
-    Path targetPath = getNavigator().showFileDialog(FileDialogType.SAVE_FILE, FileFormat.ROSE);
-    if (targetPath != null) {
-      project.save(targetPath);
-      // TODO handle failing save
+    if (!getStorageLock().isStorageLockAcquired()) {
+      getStorageLock().acquireStorageLock();
+      this.onProjectIoActionBeginCallbacks.forEach(Runnable::run);
+      Path targetFilePath
+          = getNavigator().showFileDialog(FileDialogType.SAVE_FILE, FileFormat.ROSE);
+
+      if (targetFilePath != null) {
+        boolean savingSucceeded = project.save(targetFilePath);
+        if (!savingSucceeded) {
+          getNavigator().showErrorDialog(ErrorType.SAVE_ERROR);
+        }
+      }
+      this.onProjectIoActionEndCallbacks.forEach(Runnable::run);
     }
   }
 
   @Override
   public void loadBackup(Path backUpPath) {
+    if (!getStorageLock().isStorageLockAcquired()) {
+      getStorageLock().acquireStorageLock();
+      this.onProjectIoActionBeginCallbacks.forEach(Runnable::run);
 
+      boolean loadingSucceeded = project.load(backUpPath);
+      if (!loadingSucceeded) {
+        getNavigator().showErrorDialog(ErrorType.LOAD_ERROR);
+      }
+
+      this.onProjectIoActionEndCallbacks.forEach(Runnable::run);
+    }
   }
 
   @Override
   public void subscribeToProjectIoAction(Runnable onBegin, Runnable onDone) {
-
+    this.onProjectIoActionBeginCallbacks.add(onBegin);
+    this.onProjectIoActionEndCallbacks.add(onDone);
   }
 
   @Override
   public void unsubscribeFromProjectIoAction(Runnable onBegin, Runnable onEnd) {
-
+    this.onProjectIoActionBeginCallbacks.remove(onBegin);
+    this.onProjectIoActionEndCallbacks.remove(onEnd);
   }
 }

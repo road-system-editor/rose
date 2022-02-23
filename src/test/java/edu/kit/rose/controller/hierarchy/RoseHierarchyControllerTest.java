bf490e4df193b5e98cc03ef8f7ac41bc8f9cc83e
cgoru <cgorun2000@gmail.com>
2022-02-23 21:23:52 +0100

    Added new unit tests for RoseHierarchyController

--- src/test/java/edu/kit/rose/controller/hierarchy/RoseHierarchyControllerTest.java
+++ src/test/java/edu/kit/rose/controller/hierarchy/RoseHierarchyControllerTest.java
@@ -5,34 +5,44 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import edu.kit.rose.controller.command.RoseChangeCommandBuffer;
 import edu.kit.rose.controller.commons.ReplacementLog;
 import edu.kit.rose.controller.commons.RoseStorageLock;
 import edu.kit.rose.controller.navigation.Navigator;
 import edu.kit.rose.controller.selection.RoseSelectionBuffer;
+import edu.kit.rose.controller.selection.SelectionBuffer;
 import edu.kit.rose.model.ModelFactory;
 import edu.kit.rose.model.Project;
 import edu.kit.rose.model.roadsystem.RoadSystem;
+import edu.kit.rose.model.roadsystem.elements.Base;
 import edu.kit.rose.model.roadsystem.elements.Element;
 import edu.kit.rose.model.roadsystem.elements.Group;
+import edu.kit.rose.model.roadsystem.elements.Segment;
 import edu.kit.rose.model.roadsystem.elements.SegmentType;
 import edu.kit.rose.util.RoadSystemUtility;
 import java.nio.file.Path;
 import java.util.Set;
+import java.util.function.BiConsumer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 /**
  * Unit test for {@link RoseHierarchyController}.
  */
 class RoseHierarchyControllerTest {
   private static final Path CONFIG_PATH = Path.of("build/tmp/no-config.json");
+  private static final Boolean SELECTED = true;
+  private static final Boolean UNSELECTED = false;
 
   private RoadSystem roadSystem;
   private Group group;
   private Element element;
-
+  private SelectionBuffer selectionBuffer;
   private RoseHierarchyController controller;
 
   @BeforeEach
@@ -42,9 +52,10 @@ class RoseHierarchyControllerTest {
     this.roadSystem = project.getRoadSystem();
     this.element = roadSystem.createSegment(SegmentType.BASE);
     this.group = roadSystem.createGroup(Set.of());
+    this.selectionBuffer = new RoseSelectionBuffer();
 
     this.controller = new RoseHierarchyController(new RoseStorageLock(),
-            new RoseChangeCommandBuffer(), new RoseSelectionBuffer(), project,
+            new RoseChangeCommandBuffer(), this.selectionBuffer, project,
             mock(Navigator.class), new ReplacementLog());
   }
 
@@ -71,5 +82,60 @@ class RoseHierarchyControllerTest {
     var groupName = "my favourite group";
     this.controller.setGroupName(this.group, groupName);
     assertEquals(groupName, this.group.getName());
+  }
+
+  @Test
+  void toggleSegmentSelectionTest() {
+    Segment segment = new Base();
+
+    this.controller.toggleSegmentSelection(segment);
+    Assertions.assertTrue(this.selectionBuffer.getSelectedSegments().contains(segment));
+    this.controller.toggleSegmentSelection(segment);
+    Assertions.assertFalse(this.selectionBuffer.getSelectedSegments().contains(segment));
+  }
+
+  @Test
+  void addSegmentSelectionTest() {
+    Segment segment = new Base();
+
+    this.controller.addSegmentSelection(segment);
+    Assertions.assertTrue(this.selectionBuffer.getSelectedSegments().contains(segment));
+  }
+
+  @Test
+  void removeSegmentSelectionTest() {
+    Segment segment = new Base();
+
+    this.controller.addSegmentSelection(segment);
+    Assertions.assertTrue(this.selectionBuffer.getSelectedSegments().contains(segment));
+    this.controller.removeSegmentSelection(segment);
+    Assertions.assertFalse(this.selectionBuffer.getSelectedSegments().contains(segment));
+  }
+
+  @Test
+  void clearSegmentSelectionTest() {
+    Segment segment1 = new Base();
+    Segment segment2 = new Base();
+
+    this.controller.addSegmentSelection(segment1);
+    this.controller.addSegmentSelection(segment2);
+    this.controller.clearSegmentSelection();
+    Assertions.assertFalse(this.selectionBuffer.getSelectedSegments().contains(segment1));
+    Assertions.assertFalse(this.selectionBuffer.getSelectedSegments().contains(segment2));
+  }
+
+  @Test
+  void subscriptionAndNotifyTest() {
+    Segment segment = new Base();
+    BiConsumer<Segment, Boolean> consumer = mock(BiConsumer.class);
+
+    this.controller.addSubscription(consumer);
+    this.controller.notifyAddition(segment);
+    verify(consumer, Mockito.times(1)).accept(segment, SELECTED);
+    this.controller.notifyRemoval(segment);
+    verify(consumer, times(1)).accept(segment, UNSELECTED);
+    this.controller.removeSubscription(consumer);
+    this.controller.notifyAddition(segment);
+    verify(consumer, times(1)).accept(segment, SELECTED);
   }
 }

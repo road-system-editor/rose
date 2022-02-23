845617313b95cc20658265efc7f769232cb28d63
Max Schweikart <hello@maxschweik.art>
2022-02-23 14:57:58 +0100

    test(infrastructure): add tests for Movement#equals and Movement#hashCode

--- src/test/java/edu/kit/rose/infrastructure/MovementTest.java
+++ src/test/java/edu/kit/rose/infrastructure/MovementTest.java
@@ -1,26 +1,59 @@
 package edu.kit.rose.infrastructure;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
  * Unit Tests for the {@link Movement} Class.
  */
 public class MovementTest {
-  private static final int FirstTestInt = 7;
-  private static final int SecondTestInt = 9;
+  private static final int MOVEMENT_X = 7;
+  private static final int MOVEMENT_Y = 9;
 
-  @Test
-  public void testGetCoordinates() {
-    Movement movement = new Movement(FirstTestInt, SecondTestInt);
-    Assertions.assertEquals(FirstTestInt, movement.getX());
-    Assertions.assertEquals(SecondTestInt, movement.getY());
+  private Movement movement;
+
+  @BeforeEach
+  void beforeEach() {
+    this.movement = new Movement(MOVEMENT_X, MOVEMENT_Y);
   }
 
   @Test
-  public void testStandardConstructor() {
+  void testGetCoordinates() {
+    assertEquals(MOVEMENT_X, this.movement.getX());
+    assertEquals(MOVEMENT_Y, this.movement.getY());
+  }
+
+  @Test
+  void testStandardConstructor() {
     Movement movement = new Movement();
-    Assertions.assertEquals(0, movement.getX());
-    Assertions.assertEquals(0, movement.getY());
+    assertEquals(0, movement.getX());
+    assertEquals(0, movement.getY());
+  }
+
+  @Test
+  void testEquals() {
+    var equalMovement = new Movement(MOVEMENT_X, MOVEMENT_Y);
+
+    assertEquals(equalMovement, movement);
+    assertEquals(equalMovement.hashCode(), movement.hashCode());
+  }
+
+  @Test
+  void testEqualsWithUnequalObjects() {
+    //noinspection ConstantConditions,SimplifiableAssertion <-- that's what we're testing
+    assertFalse(movement.equals(null));
+
+    //noinspection SimplifiableAssertion,EqualsBetweenInconvertibleTypes
+    assertFalse(movement.equals(new Position(MOVEMENT_X, MOVEMENT_Y)));
+
+    assertNotEquals(new Movement(MOVEMENT_X + 1, MOVEMENT_Y), movement);
+    assertNotEquals(new Movement(MOVEMENT_X, MOVEMENT_Y + 1), movement);
+    assertNotEquals(new Movement(MOVEMENT_X + 1, MOVEMENT_Y + 1), movement);
   }
 }

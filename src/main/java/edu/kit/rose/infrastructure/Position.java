261a4297359fa37d2027810f9a94e40dd1d199b3
Max Schweikart <hello@maxschweik.art>
2022-02-26 21:15:51 +0100

    refactor(infrastructure): increase readability of Position#equals

--- src/main/java/edu/kit/rose/infrastructure/Position.java
+++ src/main/java/edu/kit/rose/infrastructure/Position.java
@@ -93,16 +93,10 @@ public class Position {
 
   @Override
   public boolean equals(Object obj) {
-    if (obj == null) {
-      return false;
-    }
-
-    if (this.getClass() != obj.getClass()) {
-      return false;
-    }
-
-    Position other = (Position) obj;
-    return getX() == other.getX() && getY() == other.getY();
+    return obj != null
+        && obj.getClass() == this.getClass()
+        && ((Position) obj).getX() == this.getX()
+        && ((Position) obj).getY() == this.getY();
   }
 
   @Override

b4e96100c5739cba00a6c0fb7ffdd972e567cfa2
cgoru <cgorun2000@gmail.com>
2022-03-02 17:52:22 +0100

    implemented connect base segment

---
+++ src/test/java/edu/kit/rose/view/GuiTest.java
@@ -0,0 +1,17 @@
+package edu.kit.rose.view;
+
+import javafx.stage.Stage;
+import org.testfx.framework.junit5.ApplicationTest;
+
+/**
+ * The Test class that is used to write Gui Tests.
+ */
+public abstract class GuiTest extends ApplicationTest {
+
+  @Override
+  public void start(Stage stage) {
+    RoseApplication application = new RoseApplication();
+    application.start(stage);
+    stage.show();
+  }
+}

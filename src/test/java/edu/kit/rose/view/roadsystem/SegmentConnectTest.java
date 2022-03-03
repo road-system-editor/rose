13a2f75aa145e937544d85773734f1ba90cf795a
cgoru <cgorun2000@gmail.com>
2022-03-03 18:46:33 +0100

    implemented connect entry segment test

--- src/test/java/edu/kit/rose/view/roadsystem/SegmentConnectTest.java
+++ src/test/java/edu/kit/rose/view/roadsystem/SegmentConnectTest.java
@@ -36,32 +36,53 @@ public class SegmentConnectTest extends GuiTest {
     doubleClickOn(listCell.get(0));
     List<Node> segmentViewList = grid.getChildren()
             .stream().filter(e -> e instanceof SegmentView).toList();
-    SegmentView segmentViewBase = (SegmentView) segmentViewList.get(0);
-    SegmentView segmentViewEntrance = (SegmentView) segmentViewList.get(1);
-    drag(segmentViewEntrance).interact(() -> dropBy(-60, 80));
+    drag(segmentViewList.get(1)).interact(() -> dropBy(-60, 80));
     connectorViewList = lookup((Node node) ->
             node instanceof  ConnectorView).queryAll().stream().toList();
   }
 
-  @Disabled
+  @Disabled("disabled till the pipeline will be adapted")
   @Test
   void testConnectBaseSegment() {
-    drag(connectorViewList.get(4)).interact(() ->
-            moveTo(connectorViewList.get(1)).moveBy(-1, 17).drop());
+    ConnectorView connectorView1 = (ConnectorView) connectorViewList.get(4);
+    ConnectorView connectorView2 = (ConnectorView) connectorViewList.get(1);
+    drag(connectorView1).interact(() ->
+            moveTo(connectorView2).moveBy(-1, 17).drop());
     List<Node> connectionViewList = grid.getChildren()
             .stream().filter(e -> e instanceof ConnectionView).toList();
     ConnectionView connectionView = (ConnectionView) connectionViewList.get(0);
     Assertions.assertEquals(1, connectionViewList.size());
     Rectangle connection = (Rectangle) connectionView.getChildren()
             .stream().filter(e -> e instanceof Rectangle).findFirst().orElseThrow();
-    ConnectorView connectorView1 = (ConnectorView) connectorViewList.get(4);
-    ConnectorView connectorView2 = (ConnectorView) connectorViewList.get(1);
     Point2D pos1 = connectorView1.getParent()
             .localToParent(connectorView1.getCenterX(), connectorView1.getCenterY());
     Point2D pos2 = connectorView2.getParent()
             .localToParent(connectorView2.getCenterX(), connectorView2.getCenterY());
-    Assertions.assertEquals(connectorViewList.get(4).getLayoutX(), connection.getLayoutX());
-    Assertions.assertEquals(connectorViewList.get(4).getLayoutY(), connection.getLayoutY());
+    Assertions.assertEquals(connectorView1.getLayoutX(), connection.getLayoutX());
+    Assertions.assertEquals(connectorView1.getLayoutY(), connection.getLayoutY());
+    double distance = pos1.distance(pos2);
+    Assertions.assertEquals(distance, connection.getWidth());
+  }
+
+  @Disabled("disabled till the pipeline will be adapted")
+  @Test
+  void testConnectEntrySegment() {
+    ConnectorView connectorView1 = (ConnectorView) connectorViewList.get(4);
+    ConnectorView connectorView2 = (ConnectorView) connectorViewList.get(1);
+    drag(connectorView2).interact(() ->
+            moveTo(connectorView1).moveBy(1, -17).drop());
+    List<Node> connectionViewList = grid.getChildren()
+            .stream().filter(e -> e instanceof ConnectionView).toList();
+    ConnectionView connectionView = (ConnectionView) connectionViewList.get(0);
+    Assertions.assertEquals(1, connectionViewList.size());
+    Rectangle connection = (Rectangle) connectionView.getChildren()
+            .stream().filter(e -> e instanceof Rectangle).findFirst().orElseThrow();
+    Point2D pos1 = connectorView1.getParent()
+            .localToParent(connectorView1.getCenterX(), connectorView1.getCenterY());
+    Point2D pos2 = connectorView2.getParent()
+            .localToParent(connectorView2.getCenterX(), connectorView2.getCenterY());
+    Assertions.assertEquals(connectorView2.getLayoutX(), connection.getLayoutX());
+    Assertions.assertEquals(connectorView2.getLayoutY(), connection.getLayoutY());
     double distance = pos1.distance(pos2);
     Assertions.assertEquals(distance, connection.getWidth());
   }

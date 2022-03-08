f81d4ccc226e927f8cab8fba5e15d39dc9a0e7d1
Max Schweikart <hello@maxschweik.art>
2022-03-08 21:19:38 +0100

    fix(attribute): handle invalid TextFieldAttribute input more user-friendly
    The TextFieldAttribute now marks the content as red if it is invalid, instead of resetting it

--- src/main/java/edu/kit/rose/view/panel/segment/TextFieldAttribute.java
+++ src/main/java/edu/kit/rose/view/panel/segment/TextFieldAttribute.java
@@ -4,11 +4,9 @@ import edu.kit.rose.controller.attribute.AttributeController;
 import edu.kit.rose.model.roadsystem.attributes.AttributeAccessor;
 import edu.kit.rose.view.commons.FxmlContainer;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Objects;
-import java.util.concurrent.Callable;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Function;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
@@ -20,26 +18,27 @@ import javafx.scene.control.TextField;
 public abstract class TextFieldAttribute<T> extends EditableAttribute<T> {
   private static final String ATTRIBUTE_PANEL_STYLE =
       "/edu/kit/rose/view/panel/segment/AttributePanel.css";
+  private static final String INVALID_VALUE_STYLE_CLASS = "invalid-value";
+
   /**
    * This attribute is {@code true} when this component is currently trying to insert an
    * "inhomogeneous" value into the input field and the input should not be validated.
    */
   private boolean inhomogeneousInsertion = false;
 
-
-  private TextField inputField; //TODO make final and init immediately
+  private TextField inputField;
 
   /**
    * Creates an editable attribute component for a given attribute accessor.
    */
   protected TextFieldAttribute(AttributeAccessor<T> attribute,
-                               AttributeController controller, BiConsumer<AttributeAccessor<T>,
-                                                              T> consumer) {
+                               AttributeController controller,
+                               BiConsumer<AttributeAccessor<T>, T> consumer) {
     super(attribute, controller, consumer);
-    setupView();
+    setupTextFieldAttributeStyle();
   }
 
-  private void setupView() {
+  private void setupTextFieldAttributeStyle() {
     String attributeStyleSheetUrl =
         Objects.requireNonNull(getClass().getResource(ATTRIBUTE_PANEL_STYLE)).toExternalForm();
     this.getStylesheets().add(attributeStyleSheetUrl);
@@ -77,10 +76,24 @@ public abstract class TextFieldAttribute<T> extends EditableAttribute<T> {
                                   String oldVal,
                                   String newVal) {
     if (!inhomogeneousInsertion) {
-      if (this.validate(newVal)) {
+      boolean valid = this.validate(newVal);
+      setValid(valid);
+
+      if (valid) {
         this.consumer.accept(getAttribute(), parse(newVal));
+      }
+    }
+  }
+
+  private void setValid(boolean valid) {
+    boolean invalid = !valid;
+    boolean hasInvalidClass = this.inputField.getStyleClass().contains(INVALID_VALUE_STYLE_CLASS);
+
+    if (invalid != hasInvalidClass) {
+      if (invalid) {
+        this.inputField.getStyleClass().add(INVALID_VALUE_STYLE_CLASS);
       } else {
-        inputField.setText(oldVal);
+        this.inputField.getStyleClass().remove(INVALID_VALUE_STYLE_CLASS);
       }
     }
   }
@@ -99,10 +112,11 @@ public abstract class TextFieldAttribute<T> extends EditableAttribute<T> {
     } else {
       this.inputField.setText(newValue.toString());
     }
+    setValid(true);
   }
 
   @Override
   protected Collection<FxmlContainer> getSubFxmlContainer() {
-    return null;
+    return Collections.emptyList();
   }
 }

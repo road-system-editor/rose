86500c6c45c5e3ac39afaaf680a06ce650171784
cgoru <cgorun2000@gmail.com>
2022-03-10 21:35:35 +0100

    Enabled tests for windows

--- src/test/java/edu/kit/rose/view/window/MenuTest.java
+++ src/test/java/edu/kit/rose/view/window/MenuTest.java
@@ -5,6 +5,8 @@ import javafx.scene.control.TableView;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.testfx.api.FxAssert;
 import org.testfx.matcher.control.LabeledMatchers;
 
@@ -15,11 +17,10 @@ import org.testfx.matcher.control.LabeledMatchers;
  */
 public class MenuTest extends GuiTest {
 
-  @BeforeEach
-  void setUp() {
-
-  }
-
+  /**
+   * Represents T15.
+   */
+  @EnabledOnOs(OS.WINDOWS)
   @Test
   void testChangeLanguage() {
     clickOn("#language").clickOn("#englishLanguage");
@@ -28,6 +29,10 @@ public class MenuTest extends GuiTest {
     FxAssert.verifyThat("#createGroupButton", LabeledMatchers.hasText("Group from selection"));
   }
 
+  /**
+   * Represents T27.
+   */
+  @EnabledOnOs(OS.WINDOWS)
   @Test
   void testShortcutsDisplay() {
     clickOn("#help").clickOn("#shortcuts");

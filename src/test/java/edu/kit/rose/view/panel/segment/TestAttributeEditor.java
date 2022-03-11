0c4c253d33d182bd3c48bcf8cce5f84895ce33d5
cgoru <cgorun2000@gmail.com>
2022-03-11 21:23:41 +0100

    fix the setUp method in TestAttributeEditor

--- src/test/java/edu/kit/rose/view/panel/segment/TestAttributeEditor.java
+++ src/test/java/edu/kit/rose/view/panel/segment/TestAttributeEditor.java
@@ -24,7 +24,7 @@ import org.junit.jupiter.api.condition.OS;
  * Tests the attribute/bulk editor of the GUI.
  */
 public class TestAttributeEditor extends GuiTest {
-  private List<SegmentView> segmentViewList;
+  private List<? extends SegmentView<?>> segmentViewList;
 
   @BeforeEach
   void setUp() {
@@ -34,10 +34,14 @@ public class TestAttributeEditor extends GuiTest {
             .<SegmentBlueprint>queryAll().stream().toList();
     Grid grid = lookup((Node node) -> node instanceof Grid).query();
     doubleClickOn(segmentBoxListCell.get(1));
+    segmentViewList = grid.getChildren()
+            .stream().filter(e -> e instanceof SegmentView)
+            .map(node -> (SegmentView<?>) node).toList();
+    drag(segmentViewList.get(0)).interact(() -> dropBy(0, 100));
     doubleClickOn(segmentBoxListCell.get(2));
     segmentViewList = grid.getChildren()
-            .stream().filter(e -> e instanceof SegmentView).map(SegmentView.class::cast).toList();
-    drag(segmentViewList.get(1)).interact(() -> dropBy(0, 100));
+            .stream().filter(e -> e instanceof SegmentView)
+            .map(node -> (SegmentView<?>) node).toList();
   }
 
 
@@ -49,13 +53,14 @@ public class TestAttributeEditor extends GuiTest {
   void testEnterAttribute() {
     doubleClickOn(segmentViewList.get(1));
     VBox attributeList = lookup("#attributeList").query();
-    EditableAttribute nameAttribute1 = (EditableAttribute) attributeList.getChildren().get(0);
+    EditableAttribute<?> nameAttribute1 = (EditableAttribute<?>) attributeList.getChildren().get(0);
     moveTo(nameAttribute1).moveBy(100, 0).press(MouseButton.PRIMARY).release(MouseButton.PRIMARY);
     for (int i = 0; i < 8; i++) {
       push(KeyCode.BACK_SPACE);
     }
     push(KeyCode.A);
-    EditableAttribute lengthAttribute1 = (EditableAttribute) attributeList.getChildren().get(2);
+    EditableAttribute<?> lengthAttribute1 =
+            (EditableAttribute<?>) attributeList.getChildren().get(2);
     moveTo(lengthAttribute1).moveBy(100, 0).press(MouseButton.PRIMARY).release(MouseButton.PRIMARY);
     push(KeyCode.BACK_SPACE).push(KeyCode.BACK_SPACE);
     push(KeyCode.DIGIT1);
@@ -63,8 +68,9 @@ public class TestAttributeEditor extends GuiTest {
     press(MouseButton.PRIMARY).release(MouseButton.PRIMARY);
     doubleClickOn(segmentViewList.get(1));
     attributeList = lookup("#attributeList").query();
-    EditableAttribute nameAttribute2 = (EditableAttribute) attributeList.getChildren().get(0);
-    EditableAttribute lengthAttribute2 = (EditableAttribute) attributeList.getChildren().get(2);
+    EditableAttribute<?> nameAttribute2 = (EditableAttribute<?>) attributeList.getChildren().get(0);
+    EditableAttribute<?> lengthAttribute2 =
+            (EditableAttribute<?>) attributeList.getChildren().get(2);
     Assertions.assertEquals("a", ((TextField)
             ((HBox) nameAttribute2.getChildren().get(0)).getChildren().get(1)).getText());
     Assertions.assertEquals("1", ((TextField)
@@ -81,19 +87,19 @@ public class TestAttributeEditor extends GuiTest {
     press(KeyCode.CONTROL).clickOn(segmentViewList.get(1))
             .doubleClickOn(segmentViewList.get(0)).release(KeyCode.CONTROL);
     VBox attributeList = lookup("#attributeList").query();
-    EditableAttribute lanesAttribute = (EditableAttribute) attributeList.getChildren().get(2);
+    EditableAttribute<?> lanesAttribute = (EditableAttribute<?>) attributeList.getChildren().get(2);
     ((TextField) ((HBox) lanesAttribute.getChildren().get(0)).getChildren().get(1)).setText("3");
 
     doubleClickOn(segmentViewList.get(1));
     attributeList = lookup("#attributeList").query();
-    lanesAttribute = (EditableAttribute) attributeList.getChildren().get(4);
+    lanesAttribute = (EditableAttribute<?>) attributeList.getChildren().get(4);
     Assertions.assertEquals("3", ((TextField)
             ((HBox) lanesAttribute.getChildren().get(0)).getChildren().get(1)).getText());
 
     moveBy(-100, 0).press(MouseButton.PRIMARY).release(MouseButton.PRIMARY);
     doubleClickOn(segmentViewList.get(0));
     attributeList = lookup("#attributeList").query();
-    lanesAttribute = (EditableAttribute) attributeList.getChildren().get(4);
+    lanesAttribute = (EditableAttribute<?>) attributeList.getChildren().get(4);
     Assertions.assertEquals("3", ((TextField)
             ((HBox) lanesAttribute.getChildren().get(0)).getChildren().get(1)).getText());
   }
